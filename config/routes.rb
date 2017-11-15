2b92e9811342069254fc8422af84de90b63866bd
github username <github email address>
2017-11-15 15:39:08 -0500

    nested the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :categories, only: [:index, :update, :new, :create, :show]
-      resources :posts, only: [:index, :update, :new, :create]
-      resources :comments, only: [:index, :update, :new, :create, :show]
+      resources :categories, only: [:index, :update, :new, :create, :show] do
+        resources :posts, only: [:index, :update, :new, :create, :show] do
+          resources :comments, only: [:index, :update, :new, :create, :show]
+        end
+      end
+
+      
       # get '/categories/id/posts/id', to: 'posts#show'
     end
   end
